7c6b175e3a0b56be8c923fa4629a0e52f72844c2
mk1-p <mk1.dev662@gmail.com>
2023-11-25 20:16:13 +0900

    CLN: Modify Bean Register - Java Factory Method & Configuration

--- src/main/java/com/example/tobyspringboot/TobySpringBootApplication.java
+++ src/main/java/com/example/tobyspringboot/TobySpringBootApplication.java
@@ -10,42 +10,61 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
 import org.springframework.boot.web.server.WebServer;
 import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.context.support.GenericApplicationContext;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
+import org.springframework.web.context.support.AnnotationConfigWebApplicationContext;
 import org.springframework.web.context.support.GenericWebApplicationContext;
 import org.springframework.web.servlet.DispatcherServlet;
 
 import java.io.IOException;
 
+@Configuration
 public class TobySpringBootApplication {
+    @Bean
+    public HelloController helloController(HelloService helloService) {
+        return new HelloController(helloService);
+    }
+
+    @Bean
+    public HelloService helloService() {
+        return new SimpleHelloService();
+    }
 
     public static void main(String[] args) {
         // Container Control Object
         // Convert GenericWebApplicationContext And Use DispatcherServlet
-        GenericWebApplicationContext applicationContext = new GenericWebApplicationContext();
-        // Register Bean
-        // Similar Singleton Pattern - Call Spring Container as Singleton Registry
-        applicationContext.registerBean(HelloController.class);
-        applicationContext.registerBean(SimpleHelloService.class);
+        AnnotationConfigWebApplicationContext applicationContext = new AnnotationConfigWebApplicationContext() {
+            @Override
+            protected void onRefresh() {
+                super.onRefresh();
+
+                // Create Servlet Server Factory(Tomcat)
+                // If you want other WebServer, Modify TomcatServletWebServerFactory
+                ServletWebServerFactory serverFactory = new TomcatServletWebServerFactory();
+                // Get WebServer Object
+                // -> Dispatcher Servlet Convert!
+                WebServer webServer = serverFactory.getWebServer(servletContext -> {
+                    servletContext.addServlet("dispatcherServlet",
+                            new DispatcherServlet(this))
+                            .addMapping("/*");
+                });
+                // Run WebServer
+                webServer.start();
+
+            }
+        };
+        applicationContext.register(TobySpringBootApplication.class);
         // Refresh And Create Bean Object
         applicationContext.refresh();
 
 
-        // Create Servlet Server Factory(Tomcat)
-        // If you want other WebServer, Modify TomcatServletWebServerFactory
-        ServletWebServerFactory serverFactory = new TomcatServletWebServerFactory();
-        // Get WebServer Object
-        // -> Dispatcher Servlet Convert!
-        WebServer webServer = serverFactory.getWebServer(servletContext -> {
-            servletContext.addServlet("dispatcherServlet",
-                    new DispatcherServlet(applicationContext)
-            ).addMapping("/*");
-        });
-        // Run WebServer
-        webServer.start();
+
     }
 
 }
