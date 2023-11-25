81b3a3a77a3c61e14780cb784590f0df01fe8a6f
mk1-p <mk1.dev662@gmail.com>
2023-11-25 17:01:09 +0900

    ENH: First Commit

---
+++ src/main/java/com/example/tobyspringboot/TobySpringBootApplication.java
@@ -0,0 +1,42 @@
+package com.example.tobyspringboot;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
+import org.springframework.boot.web.server.WebServer;
+import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+
+import java.io.IOException;
+
+public class TobySpringBootApplication {
+
+    public static void main(String[] args) {
+        // Create Servlet Server Factory(Tomcat)
+        // If you want other WebServer, Modify TomcatServletWebServerFactory
+        ServletWebServerFactory serverFactory = new TomcatServletWebServerFactory();
+        // Get WebServer Object
+        WebServer webServer = serverFactory.getWebServer(servletContext -> {
+            servletContext.addServlet("hello", new HttpServlet() {
+                @Override
+                protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+
+                    String name = req.getParameter("name");
+
+                    resp.setStatus(HttpStatus.OK.value());
+                    resp.setHeader(HttpHeaders.CONTENT_TYPE, MediaType.TEXT_PLAIN_VALUE);
+                    resp.getWriter().print("Hello Servlet! "+name);
+                }
+            }).addMapping("/hello");
+        });
+        // Run WebServer
+        webServer.start();
+    }
+
+}
