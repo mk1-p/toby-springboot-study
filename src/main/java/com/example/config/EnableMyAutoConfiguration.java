49a6ede516370b9c7f724e4fb7df89aa76183bf6
mk1-p <mk1.dev662@gmail.com>
2023-11-26 20:41:01 +0900

    ENH: Add MyAutoConfigImportSelector

--- src/main/java/com/example/config/EnableMyAutoConfiguration.java
+++ src/main/java/com/example/config/EnableMyAutoConfiguration.java
@@ -12,6 +12,6 @@ import java.lang.annotation.Target;
 @Retention(RetentionPolicy.RUNTIME)
 @Target(ElementType.TYPE)
 // ComponentScan 범위 외에 있어도 Import 를 시켜주면 인식하게끔된다.
-@Import({DispatcherServletConfig.class, TomcatWebServerConfig.class})
+@Import(MyAutoConfigImportSelector.class)
 public @interface EnableMyAutoConfiguration {
 }
