0c9bdc5b9fe3c43ce51d9d94d49fca2607d7cd47
mk1-p <mk1.dev662@gmail.com>
2023-11-27 17:13:22 +0900

    CLN: Modify Configuration Elements And Config File Annotation

--- src/main/java/com/example/config/MyAutoConfiguration.java
+++ src/main/java/com/example/config/MyAutoConfiguration.java
@@ -10,6 +10,6 @@ import java.lang.annotation.Target;
 
 @Retention(RetentionPolicy.RUNTIME)
 @Target(ElementType.TYPE)
-@Configuration
+@Configuration(proxyBeanMethods = false)    // ConfigurationTest 파일 참고! 등록될 Bean 객체에 프록시를 만들지 않는다는 옵션
 public @interface MyAutoConfiguration {
 }
