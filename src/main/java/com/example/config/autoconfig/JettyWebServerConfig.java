4f3b426511a19b012f582b53a8c30f26ab51fe00
mk1-p <mk1.dev662@gmail.com>
2023-11-27 19:07:11 +0900

    ENH: Add Jetty Config

--- src/main/java/com/example/config/autoconfig/JettyWebServerConfig.java
+++ src/main/java/com/example/config/autoconfig/JettyWebServerConfig.java
@@ -1,17 +1,18 @@
 package com.example.config.autoconfig;
 
 import com.example.config.MyAutoConfiguration;
+import org.springframework.boot.web.embedded.jetty.JettyServletWebServerFactory;
 import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
 import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
 
 @MyAutoConfiguration
-public class TomcatWebServerConfig {
+public class JettyWebServerConfig {
 
-    @Bean("tomcatWebServerFactory")
+    // 보통은 Bean의 이름은 메소드 이름을 따라간다.
+    @Bean("jettyWebServerFactory")
     public ServletWebServerFactory servletWebServerFactory() {
-        return new TomcatServletWebServerFactory();
+        return new JettyServletWebServerFactory();
     }
 
 }
