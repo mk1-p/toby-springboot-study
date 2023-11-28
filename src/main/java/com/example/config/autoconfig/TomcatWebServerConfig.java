bb5b3be88ef231af22bdf0de08838efd2fd53f1a
mk1-p <mk1.dev662@gmail.com>
2023-11-28 18:18:51 +0900

    CLN: Modify Tomcat User Options

--- src/main/java/com/example/config/autoconfig/TomcatWebServerConfig.java
+++ src/main/java/com/example/config/autoconfig/TomcatWebServerConfig.java
@@ -6,6 +6,7 @@ import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean
 import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
 import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
 import org.springframework.context.annotation.*;
+import org.springframework.core.env.Environment;
 import org.springframework.core.type.AnnotatedTypeMetadata;
 import org.springframework.util.ClassUtils;
 
@@ -15,8 +16,12 @@ public class TomcatWebServerConfig {
 
     @Bean("tomcatWebServerFactory")
     @ConditionalOnMissingBean   // 해당 반환타입의 Bean이 등록되어있는지 체크하고 없으면 해당 Bean을 사용
-    public ServletWebServerFactory servletWebServerFactory() {
-        return new TomcatServletWebServerFactory();
+    public ServletWebServerFactory servletWebServerFactory(Environment env) {
+        TomcatServletWebServerFactory serverFactory = new TomcatServletWebServerFactory();
+        // RequestPath 의 Prefix 를 지정
+        serverFactory.setContextPath(env.getProperty("contextPath"));
+        return serverFactory;
+//        return new TomcatServletWebServerFactory();
     }
 
 }
--- src/main/java/com/example/tobyspringboot/TobySpringBootApplication.java
+++ src/main/java/com/example/tobyspringboot/TobySpringBootApplication.java
@@ -1,12 +1,28 @@
 package com.example.tobyspringboot;
 
 import com.example.config.MySpringBootApplication;
+import org.springframework.boot.ApplicationArguments;
+import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.core.env.Environment;
 
 @MySpringBootApplication
 public class TobySpringBootApplication {
 
-
+    @Bean
+    ApplicationRunner applicationRunner(Environment env) {
+        return args -> {
+            // 만약 Environment Variable 설정을 넣는다면
+            // 우선순위상 E.V 가 application properties 보다 높기 때문에 E.V 값이 설정된다.
+            // MY_NAME Key 값으로 넣어도 my.name의 여러 경우의 수를 인식하기 때문에
+            // 몇가지 명명법에 맞게 넣는다면 적용된다.
+            // System Properties 를 적용하는 경우 우선 순위 상 S.P가 적용된다.
+            // ex. -Dmy.name=SystemProperty
+            String name = env.getProperty("my.name");
+            System.out.println("my.name = "+name);
+        };
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(TobySpringBootApplication.class, args);
