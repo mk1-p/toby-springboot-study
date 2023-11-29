8fcd653189677d256492506f88dac98c902d420a
mk1-p <mk1.dev662@gmail.com>
2023-11-29 15:59:08 +0900

    ENH: Set @Value default value

--- src/main/java/com/example/config/autoconfig/TomcatWebServerConfig.java
+++ src/main/java/com/example/config/autoconfig/TomcatWebServerConfig.java
@@ -14,8 +14,12 @@ public class TomcatWebServerConfig {
 
     // application property 값을 호출함
     // 작동원리는 PropertySourcesPlaceholderConfigurer 클래스 참고
-    @Value("${contextPath}")
+    @Value("${contextPath:}")
     private String contextPath;
+    // Placeholder 에 빈 값이면 null을 반환하기에
+    // default 값을 지정하고 싶다면 ${key:default value} 를 넣어주면 된다.
+    @Value("${port:8080}")
+    int port;
 
     @Bean("tomcatWebServerFactory")
     @ConditionalOnMissingBean   // 해당 반환타입의 Bean이 등록되어있는지 체크하고 없으면 해당 Bean을 사용
@@ -23,6 +27,7 @@ public class TomcatWebServerConfig {
         TomcatServletWebServerFactory serverFactory = new TomcatServletWebServerFactory();
         // RequestPath 의 Prefix 를 지정
         serverFactory.setContextPath(this.contextPath);
+        serverFactory.setPort(port);
         return serverFactory;
     }
 
