e90d9b88b6faacd4186f482019ab872df1503fdb
mk1-p <mk1.dev662@gmail.com>
2023-12-01 20:49:46 +0900

    ENH: Add Conditional Report console log

--- src/main/java/com/example/tobyspringboot/TobySpringBootApplication.java
+++ src/main/java/com/example/tobyspringboot/TobySpringBootApplication.java
@@ -4,6 +4,7 @@ import jakarta.annotation.PostConstruct;
 import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.condition.ConditionEvaluationReport;
 import org.springframework.context.annotation.Bean;
 import org.springframework.core.env.Environment;
 import org.springframework.jdbc.core.JdbcTemplate;
@@ -24,16 +25,18 @@ public class TobySpringBootApplication {
 
 
     @Bean
-    ApplicationRunner applicationRunner(Environment env) {
+    ApplicationRunner applicationRunner(Environment env, ConditionEvaluationReport report) {
+        // Condition 조건에 부합하는 로그 출력 코드
         return args -> {
-            // 만약 Environment Variable 설정을 넣는다면
-            // 우선순위상 E.V 가 application properties 보다 높기 때문에 E.V 값이 설정된다.
-            // MY_NAME Key 값으로 넣어도 my.name의 여러 경우의 수를 인식하기 때문에
-            // 몇가지 명명법에 맞게 넣는다면 적용된다.
-            // System Properties 를 적용하는 경우 우선 순위 상 S.P가 적용된다.
-            // ex. -Dmy.name=SystemProperty
-            String name = env.getProperty("my.name");
-            System.out.println("my.name = "+name);
+            report.getConditionAndOutcomesBySource().entrySet().stream()
+                    .filter(co -> co.getValue().isFullMatch())
+                    .filter(co -> co.getKey().indexOf("Jmx") < 0)
+                    .forEach(co -> {
+                        System.out.println(co.getKey());
+                        co.getValue().forEach(c -> {
+                            System.out.println("\t"+c.getOutcome());
+                        });
+                    });
         };
     }
 
